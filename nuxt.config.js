c8c664bad778fae739d73205c0fd8891c9d5b46b
takashi145 <t.shuhei0203@gmail.com>
2022-11-15 00:53:12 +0900

    add axios

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,6 +25,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    'plugins/axios'
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
@@ -45,7 +46,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/'
+    // baseURL: 'http://localhost:3000'
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
---
+++ plugins/axios.js
@@ -0,0 +1,14 @@
+export default ({ $axios }) => {
+  
+  $axios.onRequest((config) => {
+    console.log(config)
+  })
+
+  $axios.onResponse((config) => {
+    console.log(config)
+  })
+
+  $axios.onError((e) => {
+    console.log(e.response)
+  })
+}
