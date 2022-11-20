966368c0a89d790784a93b86d751f9cc4b5526c6
takashi145 <t.shuhei0203@gmail.com>
2022-11-20 12:49:37 +0900

    vuetify custom css

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,6 +21,7 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    '~/assets/sass/main.scss'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
@@ -51,6 +52,8 @@ export default {
   },
 
   vuetify: {
+    treeShake: true,
+    customVariables: ['~/assets/sass/variables.scss'],
     theme: {
       themes: {
         light: {
