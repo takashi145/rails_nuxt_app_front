f2d1fe74f28c241a30e4ef15f5c4278ce3bd26e9
takashi145 <t.shuhei0203@gmail.com>
2022-11-24 01:16:10 +0900

    home components

---
+++ components/App/AppFooter.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <v-footer
+      absolute
+      dark
+      color="black"
+    >
+      AppFooter.vue
+    </v-footer>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
---
+++ components/Home/HomeCompany.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    HomeCompacy.vue
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
