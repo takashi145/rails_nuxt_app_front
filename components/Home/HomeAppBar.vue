d3b40f0aff1f4892c42ba409474d2958da644962
takashi145 <t.shuhei0203@gmail.com>
2022-11-25 21:24:52 +0900

    toolbar, component

--- components/Home/HomeAppBar.vue
+++ components/Home/HomeAppBar.vue
@@ -7,28 +7,63 @@
     :elevation="toolbarStyle.elevation"
   >
     <app-logo />
-    <v-toolbar-title>
+    <v-toolbar-title
+      class="hidden-mobile-and-down"
+    >
       {{ appName }}
     </v-toolbar-title>
     <v-spacer />
 
-    <v-toolbar-items class="ml-2">
+    <v-toolbar-items class="ml-2 hidden-ipad-and-down">
       <v-btn
         v-for="(menu, i) in menus"
         :key="`menu-btn-${i}`"
         text
+        :class="{ 'hidden-sm-and-down': (menu.title === 'about') }"
         @click="goTo(menu.title)"
       >
         {{ $t(`menus.${menu.title}`) }}
       </v-btn>
     </v-toolbar-items>
+    <app-login-button />
+    <app-signup-button />
+
+    <v-menu
+      bottom
+      nudge-left="110"
+      nudge-width="100"
+    >
+      <template v-slot:activator="{ on }">
+        <v-app-bar-nav-icon
+          class="hidden-ipad-and-up"
+          v-on="on"
+        />
+      </template>
+      <v-list
+        dense
+        class="hidden-ipad-and-up"
+      >
+        <v-list-item
+          v-for="(menu, i) in menus"
+          :key="`menu-list-${i}`"
+          exact
+          @click="$vuetify.goTo(`#${menu.title}`)"
+        >
+          <v-list-item-title>
+            {{ $t(`menus.${menu.title}`) }}
+          </v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
   </v-app-bar>
 </template>
 
 <script>
+import AppLoginButton from '../App/AppLoginButton.vue'
+import AppSignupButton from '../App/AppSignupButton.vue'
 import AppLogo from '../ui/AppLogo.vue'
 export default {
-  components: { AppLogo },
+  components: { AppLogo, AppLoginButton, AppSignupButton },
   props: {
     menus: {
       type: Array,
