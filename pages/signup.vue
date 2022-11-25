2d12ab0e723623429b45dd1b117f53ae356d6618
takashi145 <t.shuhei0203@gmail.com>
2022-11-25 22:42:46 +0900

    signup page front

--- pages/signup.vue
+++ pages/signup.vue
@@ -6,10 +6,15 @@
       <v-form
         v-model="isValid"
       >
-        <user-form-name />
-        <user-form-email />
-        <user-form-password />
-
+        <user-form-name
+          :name.sync="params.user.name"
+        />
+        <user-form-email
+          :email.sync="params.user.email"
+        />
+        <user-form-password
+          :password.sync="params.user.password"
+        />
         <v-btn
           :disabled="!isValid"
           block
@@ -19,6 +24,9 @@
           登録する
         </v-btn>
       </v-form>
+      <v-card-text>
+      {{ params }}
+    </v-card-text>
     </template>
   </user-form-card>
 </template>
@@ -28,7 +36,8 @@ export default {
   layout: 'before-login',
   data () {
     return {
-      isValid: false
+      isValid: false,
+      params: { user: { name: '', email: '', password: '' } }
     }
   }
 }
