0e5c7dab2ac07b4e590d979ab90733b1022f4f40
takashi145 <t.shuhei0203@gmail.com>
2022-11-28 09:13:31 +0900

    signup validation

--- pages/signup.vue
+++ pages/signup.vue
@@ -4,6 +4,7 @@
       #user-form-card-content
     >
       <v-form
+        ref="form"
         v-model="isValid"
       >
         <user-form-name
@@ -17,15 +18,16 @@
         />
         <v-btn
           :disabled="!isValid"
+          :loading="loading"
           block
           color="myblue"
           class="white--text"
+          @click="signup"
         >
           登録する
         </v-btn>
       </v-form>
       <v-card-text>
-      {{ params }}
     </v-card-text>
     </template>
   </user-form-card>
@@ -37,7 +39,21 @@ export default {
   data () {
     return {
       isValid: false,
+      loading: false,
       params: { user: { name: '', email: '', password: '' } }
+    }
+  },
+  methods: {
+    signup () {
+      this.loading = true
+      setTimeout(() => {
+        this.formReset()
+        this.loading = false
+      }, 1500)
+    },
+    formReset () {
+      this.$refs.form.reset()
+      this.params = { user: { name: '', email: '', password: '' } }
     }
   }
 }
