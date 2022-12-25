48ca41087100e8eb113f4e55bba2a67f96750e3b
takashi145 <t.shuhei0203@gmail.com>
2022-12-25 13:39:15 +0900

    error page

---
+++ layouts/error.vue
@@ -0,0 +1,82 @@
+<template>
+  <v-container
+    fill-height
+  >
+    <v-row>
+      <v-col
+        cols="12"
+      >
+        <v-card-title
+          class="justify-center"
+        >
+          {{ error.statusCode }}
+        </v-card-title>
+        <v-card-text
+          class="text-center"
+        >
+          {{ errorMessage }}
+        </v-card-text>
+        <v-card-actions
+          class="justify-center"
+        >
+          <v-icon>
+            mdi-emoticon-sick-outline
+          </v-icon>
+        </v-card-actions>
+        <v-card-actions
+          class="justify-center"
+        >
+          <v-btn
+            icon
+            x-large
+            color="appblue"
+            @click="redirect"
+          >
+            <v-icon>
+              mdi-home
+            </v-icon>
+          </v-btn>
+        </v-card-actions>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  props: {
+    error: {
+      type: Object,
+      default: null
+    }
+  },
+  computed: {
+    // ログイン前後でリダイレクトパスを切り替える
+    redirectPath () {
+      const loggedInHomePath = this.$store.state.loggedIn.homePath
+      const beforeLoginHomePath = { name: 'index' }
+      return this.$auth.loggedIn() ? loggedInHomePath : beforeLoginHomePath
+    },
+
+    responsedMessage () {
+      return (this.error.response && this.error.response.statusText) ? this.error.response.statusText : this.error.message
+    },
+
+    errorMessage () {
+      const translationPath = `error.${this.responsedMessage}`
+      return this.$te(translationPath) ? this.$t(translationPath) : this.responsedMessage
+    }
+  },
+  async created () {
+    if (this.error.statusCode === 401) {
+      await this.$auth.resetVuex()
+    }
+  },
+  methods: {
+    // リダイレクトパスが現在のルートと一致している場合はリロードを行う
+    redirect () {
+      this.redirectPath.name === this.$route.name ? this.$router.go() : this.$router.push(this.redirectPath)
+    }
+  }
+}
+</script>
